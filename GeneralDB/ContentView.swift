c089f02dd68fefc384aaadf5bd2899c4d12dd3c0
PeterBien <peter.bien@gmx.de>
2024-05-09 14:42:59 +0200

    Initial Commit

---
+++ GeneralDB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeneralDB
+//
+//  Created by Peter Bien on 09.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeneralDB/GeneralDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralDBApp.swift
+//  GeneralDB
+//
+//  Created by Peter Bien on 09.05.24.
+//
+
+import SwiftUI
+
+@main
+struct GeneralDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
